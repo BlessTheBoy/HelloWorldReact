a50be1e6605526a1873e8158585fbe7f6b8a4e60
Bakare Faruq <bakarefaruq.a@gmail.com>
2020-10-29 06:07:24 -0700

    practise (11)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome'
+import Message from './components/Message'
+import Count from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React.js World, BlessTheBoy is here!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Count />
+      {/* <Message /> */}
+      {/* <Greet name="Bruce" heroName="Batman" />
+      <Greet name="Clark" heroName="Superman" />
+      <Greet name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
     </div>
   );
 }
